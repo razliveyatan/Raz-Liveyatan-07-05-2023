0edf775b7ebcc0ee8395c4413be086b7c28d4bd1
razliveyatan <razliveyatan@gmail.com>
2023-05-14 17:59:14 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
+import App from '../src/App.vue'
+import router from './router'
 import 'vue3-toastify/dist/index.css';
 
 
-import App from '../src/App.vue'
-import router from './router'
 
 const app = createApp(App)
 

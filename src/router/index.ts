d15dacfcd144bb2e6972f0207a8f87d195344848
razliveyatan <razliveyatan@gmail.com>
2023-05-14 23:13:35 +0300

    Changed router + index.ts file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '../views/MainView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/main',
-      name: 'main',
-      component: MainView
+      path: '/home',
+      name: 'home',
+      component: HomeView
     },    
     {
       path: '/favorites',
